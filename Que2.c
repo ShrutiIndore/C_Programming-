8747fa231f7efd358c1f21132f697b8eaf490afb
ShrutiIndore <shrutiindore96@gmail.com>
2025-11-02 09:52:25 -0500

    LB Assignment folder
    My 2nd assignment of LB batch

---
+++ Que2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt =0;
+
+    while(iNo>iCnt)
+    {
+        printf("*\n");
+        iNo --;
+    }
+}
+
+
+
+int main()
+{
+    int iValue =0;
+
+    printf("Enter the Number");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
