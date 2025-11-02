9071e9f2c6cff5b64bdf7b1a018576120fc744c1
ShrutiIndore <shrutiindore96@gmail.com>
2025-11-02 11:29:32 -0500

    LB folder

---
+++ program47.c
@@ -0,0 +1,29 @@
+//Factcors of 6
+
+#include<stdio.h>
+
+void DisplayFactors(int iNo)
+{
+  int iCnt =0;
+  
+  for(iCnt=1; iCnt< iNo; iCnt++) 
+  {
+    int iCnt = 0;
+      if((iNo % iCnt)==0)
+       {
+          printf("%d\n",iCnt);
+       }
+  
+  }
+}
+int main()
+{
+  int iValue = 0;
+  
+  printf("Enter  number :\n");
+  scanf("%d", &iValue);
+
+  DisplayFactors(iValue);
+  
+  return 0;
+}
